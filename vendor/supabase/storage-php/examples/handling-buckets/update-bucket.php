0d2991604c16dea0d9ebf4a80c7798c893189449
Mafe <mahcosta54@gmail.com>
2025-10-19 13:45:08 -0300

    Limpeza de código

--- vendor/supabase/storage-php/examples/handling-buckets/update-bucket.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include __DIR__.'/../header.php';
-use Supabase\Storage\StorageClient;
-
-$client = new  StorageClient($api_key, $reference_id);
-$result = $client->updateBucket('test-bucket', ['public' => true]);
-$output = json_decode($result->getBody(), true);
-print_r($output);
--- vendor/supabase/storage-php/examples/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include __DIR__.'/../vendor/autoload.php';
-
-$keys = TestSetup::env(__DIR__);
-$api_key = $keys['API_KEY'];
-$reference_id = $keys['REFERENCE_ID'];
