f8eb5268a2fcd35ae8dd58321c9d3c1b3d20bbeb
Gui <gui0307carvalho@gmail.com>
2025-10-28 22:57:11 -0300

    Teste deploy

--- api/routes.php
+++ api/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-$controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
+// Em Vercel usamos rewrite para enviar '?path=...'. Localmente continua funcionando com REQUEST_URI
+$rawPath = $_GET['path'] ?? (parse_url($_SERVER['REQUEST_URI'])['path'] ?? '');
+$controller = trim($rawPath, '/');
 
-if (!$controller) $controller = 'login';
+if ($controller === '') $controller = 'login';
 
 if (!file_exists("../src/controllers/{$controller}.controller.php")) {
   abort(404, 'Página não encontrada.');
